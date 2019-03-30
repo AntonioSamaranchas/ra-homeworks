c4733eb24d2d78d1f25c7812d8493e341a040260
AntonioSamaranchas <ma.ai.87@yandex.ru>
2019-03-30 20:14:04 +0300

    решение задачи films домашней работы func-component

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,22 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+const Stars = ({count}) => {
+  if (typeof count == Number && (count < 1 || count > 5)) {
+    return null;
+  }
+
+  const arrIndex = [];
+  for (let i = 0; i < count; i++) {
+    arrIndex.push({id: i/*shortid.generate() - не работает*/});
+  }
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      { arrIndex.map((item) => <li key={item.id}><Star /></li>) }
+    </ul>
+  );
+}
+
+Stars.defaultProps = {
+  count: 0
 }
