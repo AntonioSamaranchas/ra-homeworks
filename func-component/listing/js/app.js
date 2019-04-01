fca91529a060ee27bfcb9f33f0290ad366bc0b41
morozov.ai <morozov.ai@pecom.local>
2019-04-01 19:41:04 +0300

    решение задачи listing домашней работы func-component

--- func-component/listing/js/app.js
+++ func-component/listing/js/app.js
@@ -1,1 +1,59 @@
 'use strict';
+
+function init() {
+
+  const Listing = ({ items }) => {
+    if (!items.length) {
+      return null;
+    }
+
+    const Image = ({ href, src }) => {
+      return (
+        <div className="item-image">
+          <a href={ href } >
+            <img src={ src } />
+          </a>
+        </div>
+      )
+    }
+
+    const Details = ({ title, code, price, quantity }) => {
+      return (
+        <div className="item-details">
+          <p className="item-title">{ title }</p>
+          { (code === 'USD') && <p className="item-price">${ price }</p> }
+          { (code === 'EUR') && <p className="item-price">€{ price }</p> }
+          { (code !== 'USD' && code !== 'EUR') && <p className="item-price">{ price } { code }</p> }
+          { (quantity <= 10) && <p className="item-quantity level-low">{ quantity } left</p> }
+          { (10 < quantity && quantity <= 20) && <p className="item-quantity level-medium">{ quantity } left</p> }
+          { (quantity > 20) && <p className="item-quantity level-high">{ quantity } left</p> }
+        </div>
+      )  
+    }
+
+    return (
+      <div className="item-list">
+        { items.map((item) => <div className="item" key={ item.listing_id }>
+                                <Image href={ item.url } src={ item.MainImage.url_570xN } />
+                                <Details title={ item.title } code={ item.currency_code } price={ item.price } quantity={item.quantity} />
+                              </div>) }
+      </div>
+    )
+  }
+  
+  Listing.defaultProps = {
+    items: []
+  }
+
+  fetch('https://neto-api.herokuapp.com/etsy', {method: 'GET'})
+    .then(res => res.json())
+      .then(res => {
+        const listing = <Listing items={res} />;
+        ReactDOM.render(listing, document.getElementById('root'));
+      })
+        .catch(e => {
+          console.log(e.message);
+        });
+}
+
+init();
