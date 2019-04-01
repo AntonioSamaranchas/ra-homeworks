2704765a24040682a43239e40d00d204047a1f53
morozov.ai <morozov.ai@pecom.local>
2019-04-01 17:17:37 +0300

    решение задачи chat домашней работы func-component

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const MessageHistory = ({list}) => {
+  if (!list.length) {
+    return null;
+  } 
+  
+  const GetType = ({props}) => {
+    const Mess = (props.type === 'response') ? Response : (props.type === 'message') ? Message : Typing;
+    return <Mess from={props.from} message={props}/>
+  }
+
+  return (
+    <ul>
+      { list.map((message) => <li key={message.id}><GetType props={message} /></li>) }
+    </ul>  
+  )
+}
+
+MessageHistory.defaultProps = {
+  list: []
+}
